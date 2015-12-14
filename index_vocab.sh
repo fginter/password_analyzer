e3e6574b160c0be2a925dabcaaed7af55c3f1c88
Filip Ginter <ginter@cs.utu.fi>
2015-12-14 13:20:25 +0200

    stricter freq cutoff

--- index_vocab.sh
+++ index_vocab.sh
@@ -1,4 +1,4 @@
 #python list_vocab_nodes.py | gzip > all_vocab.txt.gz
 
 rm -f simdb/*.simdb simdb/*.cdb
-zcat all_vocab.gz | python filter_vocab.py --only | simstring -b -d simdb/pb34_wf_exc.simdb -u -s overlap
+zcat all_vocab.gz | python filter_vocab.py --only --counts=0,0,10000,1000,100 | simstring -b -d simdb/pb34_wf_exc.simdb -u -s overlap
